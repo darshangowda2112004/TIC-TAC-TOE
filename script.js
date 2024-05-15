67d1f8dc00f3643bf57cd1e8a2712c1463ca8213
darshangowda2112004 <darshangowdakrishnappa@gmail.com>
2024-05-15 10:59:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winningMessageElement = document.getElementById("winningMessage");
+const restartButton = document.getElementById("restartButton");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+
+let circleTurn;
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
+
+function startGame() {
+  circleTurn = false; // X starts first
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS, CIRCLE_CLASS);
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  winningMessageElement.classList.remove("show");
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  if (checkWin(currentClass)) {
+    endGame(currentClass === X_CLASS ? "Player 1" : "Player 2");
+  } else if (isDraw()) {
+    endGame("Draw");
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS, CIRCLE_CLASS);
+  board.classList.add(circleTurn ? CIRCLE_CLASS : X_CLASS);
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
+
+function endGame(winner) {
+  if (winner === "Draw") {
+    winningMessageTextElement.innerText = "Draw!";
+  } else {
+    winningMessageTextElement.innerText = `${winner} wins!`;
+  }
+  winningMessageElement.classList.add("show");
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS);
+  });
+}
